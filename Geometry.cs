ab1cd4be05344cb8611ddfb88e099ad491ec4d5c
Skokartong <amanda.olving@gmail.com>
2023-11-21 12:43:01 +0100

    Add project files.

---
+++ Geometry.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    public abstract class Geometry
+    {
+        // Shared properties across the classes
+        public double Radius { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Result { get; set; }
+
+        // Abstract method is used 
+        public abstract void Area();
+    }
+
+    // Inheritance
+    class Circle : Geometry
+    {
+        public Circle()
+        {
+            Radius = 4;
+        }
+
+        // Each subclass implements its own version of the shared 'Area' method
+        public override void Area()
+        {
+            Result = Math.PI * Radius * Radius;
+            Console.WriteLine("The area of the circle is: " + Result);
+        }
+    }
+
+    class Square : Geometry
+    {
+        public Square()
+        {
+            Width = 5;
+        }
+
+        public override void Area()
+        {
+            Result = Width * Width;
+            Console.WriteLine("The area of the square is: " + Result);
+        }
+    }
+
+    class Rectangle : Geometry
+    {
+        public Rectangle()
+        {
+            Width = 4;
+            Height = 6;
+        }
+        public override void Area()
+        {
+            Result = Width * Height;
+            Console.WriteLine("The area of the rectangle is: " + Result);
+        }
+    }
+
+    class Parallelogram : Geometry
+    {
+
+        public Parallelogram()
+        {
+            Width = 5;
+            Height = 12;
+        }
+        public override void Area()
+        {
+            Result = Width * Height;
+            Console.WriteLine("The area of the parallelogram is: " + Result);
+        }
+    }
+
+    class Ellipse : Geometry
+    {
+        public Ellipse()
+        {
+            Width = 3;
+            Height = 2;
+        }
+
+        public override void Area()
+        {
+            Result = Width * Height * Math.PI;
+            Console.WriteLine("The area of the ellipse is: " + Result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace Lab7
+{
+    class Program
+    {
+        // Main-method
+        static void Main(string[] args)
+        {
+            // Five geometric objects of the base class 'Geometry' are created
+            Circle circle = new Circle();
+            Square square = new Square();
+            Rectangle rectangle = new Rectangle();
+            Ellipse ellipse = new Ellipse();
+            Parallelogram parallelogram = new Parallelogram();
+
+            // The subclasses of 'Geometry' are put in an array to iterate through their respective 'Area' methods
+            Geometry[] geometric = { circle, square, rectangle, ellipse, parallelogram };
+
+            foreach (Geometry geometri in geometric)
+            {
+                geometri.Area();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
